ca8a50a311dd48f58f8c361badd132b72a8ec987
dstorer-cap <daniel.storer@capgemini.com>
2023-12-05 12:18:05 +0000

    Updated build script to install build in current directory (piclibc_build)

--- build_script_nocrt.sh
+++ build_script_nocrt.sh
@@ -3,9 +3,6 @@
 sudo rm -rf build-aarch64-linux-elf_nocrt2
 sudo mkdir build-aarch64-linux-elf_nocrt2
 cd build-aarch64-linux-elf_nocrt2
-sudo ../scripts/do-aarch64-configure-nocrt
-sudo ninja
-# sudo rm -rf /usr/local/picolibc/aarch64-linux-gnu
-# sudo ninja install
-# sudo aarch64-linux-gnu-gcc --specs=picolibc.specs -I/home/dstorer/mk-manifest/microkit/release/microkit-sdk-1.2.6/board/maaxboard/release/include --crt0=microkit  -o hello-world.elf /home/dstorer/mk-manifest/microkit/example/maaxboard/hello/hello.c
-
+sudo ../scripts/do-aarch64-configure-nocrt -Dprefix=/home/dstorer/mk-manifest/microkit/example/zcu102/hello_libc/picolibc_build
+sudo ninja 
+sudo ninja install
