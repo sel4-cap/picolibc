c5347345f5bf6d8cc6da40389ea00d6bff53b85f
Josh Felmeden <josh.felmeden@capgemini.com>
2023-12-06 15:17:32 +0000

    changed install path to 'HOME/picolibc_build'

--- build_script_nocrt.sh
+++ build_script_nocrt.sh
@@ -3,6 +3,7 @@
 sudo rm -rf build-aarch64-linux-elf_nocrt2
 sudo mkdir build-aarch64-linux-elf_nocrt2
 cd build-aarch64-linux-elf_nocrt2
-sudo ../scripts/do-aarch64-configure-nocrt -Dprefix=/home/dstorer/mk-manifest/microkit/example/zcu102/hello_libc/picolibc_build
+mkdir ${HOME}/picolibc_build
+sudo ../scripts/do-aarch64-configure-nocrt -Dprefix=${HOME}/picolibc_build
 sudo ninja 
 sudo ninja install
