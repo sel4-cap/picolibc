f0047983e2452dce8c310298ae696570b04ae3a7
Josh Felmeden <josh.felmeden@capgemini.com>
2023-12-14 15:20:57 +0000

    added readablility on shell

--- build_script_microkit.sh
+++ build_script_microkit.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-sudo rm -rf build-picolib-microkit/
-sudo mkdir -p build-picolib-microkit
-cd build-picolib-microkit
+sudo rm -rf picolib-microkit/
+sudo mkdir picolib-microkit
+cd picolib-microkit
 mkdir ${HOME}/picolibc_build
 sudo ../scripts/do-aarch64-configure-nocrt -Dprefix=${HOME}/picolibc_build
 sudo ninja 
 sudo ninja install
+
+echo "picolibc.specs found at ./picolib-microkit/picolibc.specs"
+echo "libc.a and libm.a found at ./picolib-microkit/newlib"
